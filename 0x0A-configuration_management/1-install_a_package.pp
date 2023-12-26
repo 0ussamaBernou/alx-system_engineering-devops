b619038b3e87f6a0409b5a01961b92affd088bfb
0ussamabernou <oussama.bernou@outlook.com>
2023-12-26 21:58:05 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,12 @@
 # instal flask
-class { 'python3':
-  ensure => present,
+package { 'python3.8':
+  ensure => '3.8.10'
 }
 package { 'python3-pip':
   ensure => present,
 }
-package { 'flask==2.1.0':
-  ensure   => present,
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
