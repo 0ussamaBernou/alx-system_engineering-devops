80d1c1dcbdbcb7ecb6c200e120cc256a1e20d7f1
0ussamabernou <oussama.bernou@outlook.com>
2023-12-26 21:53:52 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,9 +2,9 @@
 #class { 'python3':
 #  ensure => present,
 #}
-#package { 'python3-pip':
-#  ensure => present,
-#}
+package { 'python3-pip':
+  ensure => present,
+}
 package { 'flask==2.1.0':
   ensure   => present,
   provider => 'pip3',
