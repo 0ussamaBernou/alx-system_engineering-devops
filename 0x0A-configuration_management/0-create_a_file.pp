4e8746a9551a7ec902a51599a30b7c1ce3951547
0ussamabernou <oussama.bernou@outlook.com>
2023-12-26 21:41:51 +0100

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates the file /tmp/school with the content "I love Puppet"
 file { '/tmp/school':
-   ensure => 'present',
-   mode  => '0744',
-   owner => 'www-data',
-   group => 'www-data',
-   content => "I love Puppet\n",
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
 }
