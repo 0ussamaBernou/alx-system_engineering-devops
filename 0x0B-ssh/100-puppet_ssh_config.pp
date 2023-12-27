15254f91e7e54476a982566344f384907d9e2477
0ussamabernou <oussama.bernou@outlook.com>
2023-12-28 00:10:40 +0100

    added include stdlib

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,16 @@
 include stdlib
 
 file_line { 'Declare identity file':
-  ensure  => present,
-  path    => '~/.ssh/config',
-  line    => 'IdentityFile ~/.ssh/school',
-  replace => true,
+  ensure             => present,
+  path               => '~/.ssh/config',
+  line               => '   IdentityFile ~/.ssh/school',
+  replace            => true,
+  append_on_no_match => true
 }
 
 file_line { 'Turn off passwd auth':
-  path    => '~/.ssh/config',
-  line    => 'PasswordAuthentication no',
-  replace => true,
+  path               => '~/.ssh/config',
+  line               => '   PasswordAuthentication no',
+  replace            => true,
+  append_on_no_match => true
 }
