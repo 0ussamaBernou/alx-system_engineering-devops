84215c0c077517887bab4caeb1410c019f63e25c
0ussamabernou <oussama.bernou@outlook.com>
2023-12-28 17:24:29 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,18 +2,18 @@
 include stdlib
 
 file_line { 'Declare identity file':
-  ensure             => present,
-  path               => '~/.ssh/config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '[\s]*IdentityFile.*',
-  replace            => true,
-  append_on_no_match => true
+  ensure => present,
+  path   => '~/.ssh/config',
+  line   => '    IdentityFile ~/.ssh/school',
+# match              => '[\s]*IdentityFile.*',
+# replace            => true,
+# append_on_no_match => true
 }
 
 file_line { 'Turn off passwd auth':
-  path               => '~/.ssh/config',
-  line               => '    PasswordAuthentication no',
-  match              => '[\s]*PasswordAuthentication.*',
-  replace            => true,
-  append_on_no_match => true
+  path => '~/.ssh/config',
+  line => '    PasswordAuthentication no',
+# match              => '[\s]*PasswordAuthentication.*',
+# replace            => true,
+# append_on_no_match => true
 }
