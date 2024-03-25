d877a9f5d2e512f247bd406e54095d63687e4f54
0ussamabernou <oussama.bernou@outlook.com>
2024-03-25 14:16:02 +0000

    update

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    id = argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(f"{url}/users/{id}").json()
+    todos = requests.get(f"{url}/todos?userId={id}").json()
+
+    completed = [task.get("title") for task in todos if task.get("completed")]
+
+    print(
+        f"Employee {user.get("name")} is done with tasks({len(completed)}/{len(todos)}):"
+    )
+
+    [print(f"\t {task}") for task in completed]
