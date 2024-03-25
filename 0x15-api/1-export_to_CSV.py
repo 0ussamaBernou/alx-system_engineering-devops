ab1eb2bccdda59c85f7ffccb4c5e1149de175932
0ussamabernou <oussama.bernou@outlook.com>
2024-03-25 18:06:21 +0000

    update

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    id = argv[1]
+    link = "https://jsonplaceholder.typicode.com"
+    user = requests.get(f"{link}/users/{id}").json()
+    todos = requests.get(f"{link}/todos?userId={id}").json()
+
+    completed = [task.get("title") for task in todos if task.get("completed")]
+
+    with open(f"{id}.csv", "w") as f:
+        for task in todos:
+            f.write(
+                f'"{user.get("id")}", "{user.get("username")}", "{True if task.get("title") in completed else False}", "{task.get("title")}"\n'
+            )
