66f6564326c3c08f745abc2c9c187eae2b06ee73
0ussamabernou <oussama.bernou@outlook.com>
2024-04-15 21:33:48 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,6 @@
 
 exec { 'fix php typo':
     command => "sed -i.bak 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-    path    => ['/usr/bin', '/usr/sbin']
+    path    => ['/usr/bin', '/usr/sbin'],
+    #provider => shell,
 }
