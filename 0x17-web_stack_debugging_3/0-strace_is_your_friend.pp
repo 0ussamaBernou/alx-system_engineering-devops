10fc8d53e9bb38e863fa81e30a874e9e5a5e4f1b
0ussamabernou <oussama.bernou@outlook.com>
2024-04-15 21:34:35 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # puppet file to fix php typo in wp-settings.php
 
 exec { 'fix php typo':
-    command => "sed -i.bak 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-    path    => ['/usr/bin', '/usr/sbin'],
-    #provider => shell,
+    command  => "sed -i.bak 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+    #path    => ['/usr/bin', '/usr/sbin'],
+    provider => shell,
 }
