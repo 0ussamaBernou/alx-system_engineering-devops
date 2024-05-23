5739cd8a4ee411970f23e9339faca0bbee14366f
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2024-05-23 20:04:31 +0100

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,9 +0,0 @@
-# !(sky is the limit)
-exec {'update ulimit':
-  command  => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 4096\"/' /etc/default/nginx",
-  provider => 'shell',
-}
--> exec {'restart nginx':
-  command  => 'service nginx restart',
-  provider => 'shell',
-}
