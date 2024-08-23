f6fa18bde30178b7b312cc076aeb448c8889ad12
0ussamabernou <oussama.bernou@outlook.com>
2024-08-23 01:46:12 +0100

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,36 @@
 # increase file descriptor limit
-file { '/etc/security/limits.d/nofile.conf':
+# Set file descriptor limits for nginx user
+file { '/etc/security/limits.d/nginx.conf':
   ensure  => present,
-  content => "* soft nofile 65536\n* hard nofile 65536\n",
+  content => "nginx soft nofile 65536\nnginx hard nofile 65536\n",
   mode    => '0644',
 }
 
+# Ensure the nginx user exists
+user { 'nginx':
+  ensure => present,
+  system => true,
+}
+
+# Reload system limits
 exec { 'reload-limits':
   command     => '/sbin/sysctl -p',
   refreshonly => true,
-  subscribe   => File['/etc/security/limits.d/nofile.conf'],
+  subscribe   => File['/etc/security/limits.d/nginx.conf'],
+}
+
+# Modify nginx.service to set higher limit
+file { '/etc/systemd/system/nginx.service.d':
+  ensure => directory,
+}
+
+file { '/etc/systemd/system/nginx.service.d/limits.conf':
+  ensure  => present,
+  content => "[Service]\nLimitNOFILE=65536\n",
+  notify  => Exec['systemctl-daemon-reload'],
+}
+
+exec { 'systemctl-daemon-reload':
+  command     => '/bin/systemctl daemon-reload',
+  refreshonly => true,
 }
